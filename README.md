bab7926fad7c75008e69fb36fd1fc6ad6f6e2f4d
leon <ming5964471402wu@qq.com>
2017-09-06 19:21:09 +0800

    初次提交第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # vue-simple
+- vue数据双向绑定原理分析和简单实现
+
+
+## 博客地址
+[http://hoowgo.com/articlesDetails59ae94b288e8375d56420c96](http://hoowgo.com/articlesDetails59ae94b288e8375d56420c96)
+
+
---
+++ vue-simple.js
@@ -0,0 +1,235 @@
+function Vue (options) {
+    var self = this;
+    this.data = options.data;
+    this.methods = options.methods;
+
+    Object.keys(this.data).forEach(function(key) {
+        self.proxyKeys(key);//将 Vue.data[key] = '' 的调用方式改成 Vue[key] = ''
+    });
+
+    observe(this.data);//设置一个监听器Observer
+    new Compile(options.el, this);//Compile来做解析和绑定工作
+    options.mounted.call(this); // 所有事情处理好后执行mounted函数
+}
+
+Vue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function getter () {
+                return self.data[key];
+            },
+            set: function setter (newVal) {
+                self.data[key] = newVal;
+            }
+        });
+    }
+}
+
+function Observer(data) {
+    this.data = data;
+    this.walk(data);
+}
+
+Observer.prototype = {
+    walk: function(data) {
+        var self = this;
+        Object.keys(data).forEach(function(key) {
+            self.defineReactive(data, key, data[key]);//处理所有数据的get set
+        });
+    },
+    defineReactive: function(data, key, val) {
+        var dep = new Dep();
+        var childObj = observe(val);//递归遍历所有子属性
+        Object.defineProperty(data, key, {
+            enumerable: true,
+            configurable: true,
+            get: function getter () {
+                if (Dep.target) {//是否需要添加订阅者
+                    dep.addSub(Dep.target);// 在这里添加一个订阅者
+                }
+                return val;
+            },
+            set: function setter (newVal) {
+                if (newVal === val) {
+                    return;
+                }
+                val = newVal;
+                dep.notify();//如果数据变化，通知所有订阅者
+            }
+        });
+    }
+};
+
+function observe(value, vm) {
+    if (!value || typeof value !== 'object') {
+        return;
+    }
+    return new Observer(value);
+};
+
+function Dep () {
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();//通知更新方法
+        });
+    }
+};
+Dep.target = null;
+
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    this.value = this.get();  // 将自己添加到订阅器的操作
+}
+
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.vm.data[this.exp];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);//调用视图更新方法
+        }
+    },
+    get: function() {
+        Dep.target = this;  // 缓存自己
+        var value = this.vm.data[this.exp]  // 强制执行监听器里的get函数
+        Dep.target = null;  // 释放自己
+        return value;
+    }
+};
+
+function Compile(el, vm) {
+    this.vm = vm;
+    this.el = document.querySelector(el);
+    this.fragment = null;
+    this.init();
+}
+
+Compile.prototype = {
+    init: function () {
+        if (this.el) {
+            this.fragment = this.nodeToFragment(this.el);//将Dom元素写入fragment中
+            this.compileElement(this.fragment);//模板首次渲染
+            this.el.appendChild(this.fragment);
+        } else {
+            console.log('Dom元素不存在');
+        }
+    },
+    nodeToFragment: function (el) {
+        var fragment = document.createDocumentFragment();
+        var child = el.firstChild;
+        //console.log(child)
+        while (child) {//直到没有子节点
+            // 将Dom元素移入fragment中
+            //console.log(child)
+            //console.log(el)
+            fragment.appendChild(child);
+            child = el.firstChild
+            //
+        }
+        return fragment;
+    },
+    compileElement: function (el) {
+        var childNodes = el.childNodes;
+        var self = this;
+        [].slice.call(childNodes).forEach(function(node) {//模板引擎，我博客有相关实现文章
+            var reg = /\{\{(.*)\}\}/;
+            var text = node.textContent;
+
+            if (self.isElementNode(node)) {  
+                self.compile(node);
+            } else if (self.isTextNode(node) && reg.test(text)) {
+                self.compileText(node, reg.exec(text)[1]);
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                self.compileElement(node);
+            }
+        });
+    },
+    compile: function(node) {
+        var nodeAttrs = node.attributes;
+        var self = this;
+        Array.prototype.forEach.call(nodeAttrs, function(attr) {
+            var attrName = attr.name;
+            if (self.isDirective(attrName)) {
+                var exp = attr.value;
+                var dir = attrName.substring(2);
+                if (self.isEventDirective(dir)) {  // 事件指令
+                    self.compileEvent(node, self.vm, exp, dir);
+                } else {  // v-model 指令
+                    self.compileModel(node, self.vm, exp, dir);
+                }
+                node.removeAttribute(attrName);
+            }
+        });
+    },
+    compileText: function(node, exp) {
+        var self = this;
+        var initText = this.vm[exp];
+        this.updateText(node, initText);
+        new Watcher(this.vm, exp, function (value) {
+            self.updateText(node, value);
+        });
+    },
+    compileEvent: function (node, vm, exp, dir) {
+        var eventType = dir.split(':')[1];
+        var cb = vm.methods && vm.methods[exp];
+
+        if (eventType && cb) {
+            node.addEventListener(eventType, cb.bind(vm), false);
+        }
+    },
+    compileModel: function (node, vm, exp, dir) {
+        var self = this;
+        var val = this.vm[exp];
+        this.modelUpdater(node, val);
+        new Watcher(this.vm, exp, function (value) {
+            self.modelUpdater(node, value);
+        });
+
+        node.addEventListener('input', function(e) {
+            var newValue = e.target.value;
+            if (val === newValue) {
+                return;
+            }
+            self.vm[exp] = newValue;
+            val = newValue;
+        });
+    },
+    updateText: function (node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+    modelUpdater: function(node, value, oldValue) {
+        node.value = typeof value == 'undefined' ? '' : value;
+    },
+    isDirective: function(attr) {
+        return attr.indexOf('v-') == 0;
+    },
+    isEventDirective: function(dir) {
+        return dir.indexOf('on:') === 0;
+    },
+    isElementNode: function (node) {
+        return node.nodeType == 1;
+    },
+    isTextNode: function(node) {
+        return node.nodeType == 3;
+    }
+}
+
+
+
